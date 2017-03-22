0a7ca75969f5b107393f048cc0d71437f58627b5
Linus Casassa <lcasassa@gmail.com>
2017-03-22 12:26:42 -0300

    Actualizando telegram token.

--- bot_gae.py
+++ bot_gae.py
@@ -10,7 +10,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 global bot
-bot = telegram.Bot(token='TOKEN')
+bot = telegram.Bot(token='361644162:AAGbFjy05HbhgK5CptAp6RNPuj70-AjmU8E')
 
 
 @app.route('/HOOK', methods=['POST'])
