6345f1332aaa393ec6a6b459b820d5bcfc371c77
Linus Casassa <lcasassa@gmail.com>
2017-03-31 09:18:00 -0300

    Adding new fields to the db.

--- db.py
+++ db.py
@@ -5,6 +5,9 @@ class Post(ndb.Model):
     text = ndb.StringProperty()
     fb_id = ndb.StringProperty()
     date = ndb.DateTimeProperty(indexed = True)
+    username = ndb.StringProperty()
+    user_id = ndb.StringProperty()
+    url = ndb.StringProperty()
 
 class LastPostSent(ndb.Model):
     #chat_id = ndb.StringProperty() #using id as chat_id
